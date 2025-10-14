a4492392d30ff1305c85e7a6acd2e50a193f4747
er1casan0s <erica.santos.lima15@gmail.com>
2025-10-14 16:22:02 -0300

    Add files via upload

---
+++ pratica1.py
@@ -0,0 +1,35 @@
+# Minha primeira aplicação em Python
+print("Hello, World!")
+print("Minha primeira aplicação em Python")
+
+# Matematica com dois numeros valuaveis
+# declara as variaveis numero1 e numero2 e atribui os valores 10 e 5
+numero1=10;
+numero2=5;
+
+# realiza as operações matematicas e atribui o resultado a variaveis
+soma=numero1+numero2;
+subtracao=numero1-numero2;
+multiplicacao=numero1*numero2;
+divisao=numero1/numero2;
+
+# Usa a virgula para separar o texto do valor da variavel
+print("Soma:",soma)
+print("Subtração:",subtracao)
+print("Multiplicação:",multiplicacao)   
+print("Divisão:",divisao)
+
+# Operações com strings
+# recebe o valor "Erica" e atribui a variavel nome
+nome="Erica";
+sobrenome="Santos";
+# concatena as variaveis nome e sobrenome e atribui a variavel nomecompleto
+nomecompleto=nome+" "+sobrenome;
+# Usa a virgula para separar o texto do valor da variavel
+print("NOME COMPLETO:" ,nomecompleto)
+
+# Entrada de dados
+#cria a variavel nome e atribui o valor digitado pelo usuario e input pede a entrada de dados
+nome=input("Digite seu nome:")
+#Usa a virgula para separar o texto do valor da variavel
+print("Olá",nome)
